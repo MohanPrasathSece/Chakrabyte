db6fe486871a272d04d9157a8c56bf62aca67609
MohanPrasathSece <mohanprasath.s2023ece@sece.ac.in>
2025-12-26 08:44:41 +0530

    new

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -90,10 +90,11 @@ const SEO: React.FC<SEOProps> = ({
       <meta name="ICBM" content="13.0827, 80.2707" />
 
       {/* Favicon */}
-      <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+      <link rel="icon" type="image/png" href="/favicon-rounded.png" />
+      <link rel="icon" type="image/x-icon" href="/favicon-rounded.png" />
       <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
       <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/favicon-180x180.png" />
 
       {/* Structured Data */}
       {allStructuredData.map((data, index) => (
